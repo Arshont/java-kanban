cfffaa27070505da474ecb8e54a7b4842e4f9ed2
Fedor Budnikov <fedor.budnikov@list.ru>
2025-11-20 12:37:48 +0300

    Класс Node перенесён внутрь класса InMemoryHistoryManager

--- src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
@@ -94,4 +94,48 @@ public class InMemoryHistoryManager implements HistoryManager {
 			node.setPrev(null);
 		}
 	}
+
+	class Node {
+		private Node next;
+		private Node prev;
+		private Task value;
+
+		public Task getValue() {
+			return value;
+		}
+
+		public void setValue(Task value) {
+			if (value != null) {
+				this.value = value;
+			} else {
+				throw new NullPointerException();
+			}
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+
+		public Node getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		public Node(Task value) {
+			if (value != null) {
+				this.value = value;
+			} else {
+				throw new NullPointerException();
+			}
+		}
+
+
+	}
 }
